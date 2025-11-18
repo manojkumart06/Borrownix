d4971dc23bebc600ecb2183cdd481c4f384abb1d
manojkumart06 <manojkumar06t@gmail.com>
2025-11-18 15:52:28 +0530

    add allowed origin for Netlify deployment

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
+  "https://borrowniex.netlify.app",
   "https://borrownixx.onrender.com"   // frontend
 ];
 
